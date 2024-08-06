ee5294deb1641afdb0275c6806cfa8fe674fe112
Ferguson <fergusonpc@cdmsmith.com>
2024-08-06 17:24:30 -0500

    fixing language in notebook 1

--- input_configuration.py
+++ input_configuration.py
@@ -5,10 +5,10 @@
 
 
 # 1. Teslakit database path (local path to teslakit repo)
-p_data_input = r'C:/Users/fergusonpc/Teslakit-SF-Final'
+p_data_input = r'C:/Users/fergusonpc/teslakit'
 
 # 2. The name of the site
-site_name = 'SF_Test5'
+site_name = 'SF'
 
 # 3. Number of simulations associated with Notebook 1, Notebook 4, Notebook 11, Notebook 12, and Notebook 13. (default = 100)
 num_sims_input = 100
